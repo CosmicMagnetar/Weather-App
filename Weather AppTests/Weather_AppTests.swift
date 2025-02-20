b709f02bbf6983a2db4408e6a1663ea0c3e2ae37
Krishna <krishna.2024@nst.rishihood.edu.in>
2025-02-20 14:44:39 +0530

    Initial Commit

---
+++ Weather AppTests/Weather_AppTests.swift
@@ -0,0 +1,16 @@
+//
+//  Weather_AppTests.swift
+//  Weather AppTests
+//
+//  Created by Krishna . on 20/02/25.
+//
+
+import Testing
+
+struct Weather_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
